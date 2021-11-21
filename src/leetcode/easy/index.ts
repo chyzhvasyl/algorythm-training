ea12452837239b1d67184f0c58ea3d5bdc29a98f
Vasyl Chyzh <vasyl@backbase.com>
2021-11-21 21:42:08 +0200

    is sudoku valid

--- src/leetcode/easy/index.ts
+++ src/leetcode/easy/index.ts
@@ -3,19 +3,18 @@ import { maxProfit } from "./Best Time to Buy and Sell Stock II";
 import { rotate } from "./Rotate Array";
 import { containsDuplicate } from "./Contains Duplicate  ";
 import { singleNumber } from "./Single Number  ";
+import { intersect } from "./Intersection of Two Arrays II";
+import { twoSum } from "./Two Sum  ";
+import { isValidSudoku } from "./Valid Sudoku";
 
 export const main = (): void => {
   // const removeDuplicatesInput = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2];
   // console.log('removeDuplicates', removeDuplicates(removeDuplicatesInput));
   // console.log('---------------------------------------------------------');
 
-
-
   // const maxProfitInput = [7,1,5,3,6,4];
   // console.log('maxProfitInput', maxProfit(maxProfitInput));
   // console.log('---------------------------------------------------------');
-
-
 
   // const arr = [1,2,3,4, 5,6, 7];
   // const k = 3;
@@ -26,8 +25,38 @@ export const main = (): void => {
   // console.log('containsDuplicate', containsDuplicate(arr));
   // console.log('---------------------------------------------------------');
 
-  const arr = [1];
-  console.log('singleNumber', singleNumber(arr));
+  // const arr = [1];
+  // console.log('singleNumber', singleNumber(arr));
+  // console.log('---------------------------------------------------------');
+  //
+  // const nums1 = [1,2,2,1];
+  // const nums2 = [2,2];
+  //
+  // console.log('intersect', intersect(nums1, nums2));
+  // console.log('---------------------------------------------------------');
+
+  //
+  // const nums = [3,3];
+  // const target = 6;
+  //
+  // console.log('twoSum', twoSum(nums, target));
+  // console.log('---------------------------------------------------------');
+
+
+  const nums = [
+    ["5","3",".",".","7",".",".",".","."]
+    ,["6",".",".","1","9","5",".",".","."]
+    ,[".","9","8",".",".",".",".","6","."]
+    ,["8",".",".",".","6",".",".",".","3"]
+    ,["4",".",".","8",".","3",".",".","1"]
+    ,["7",".",".",".","2",".",".",".","6"]
+    ,[".","6",".",".",".",".","2","8","."]
+    ,[".",".",".","4","1","9",".",".","5"]
+    ,[".",".",".",".","8",".",".","7","9"]
+    ]
+  ;
+
+  console.log('isValidSudoku', isValidSudoku(nums));
   console.log('---------------------------------------------------------');
 }
 
