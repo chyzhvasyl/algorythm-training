66710f9161b82a6771114f542b43a5621b65cc57
Vasyl Chyzh <v.chyzh@levi9.com>
2023-11-17 19:09:24 +0200

    Set Matrix Zeroes: approved

--- src/leetcode/index.ts
+++ src/leetcode/index.ts
@@ -1,5 +1,7 @@
 import * as easy from "./easy";
+import * as medium from "./medium";
 
 export const leetCode = (): void => {
-  easy.main();
+  // easy.main();
+  medium.main();
 }
---
+++ src/leetcode/medium/index.ts
@@ -0,0 +1,7 @@
+import { logger } from '../../shared';
+import { setZeroes } from './Set Matrix Zeroes';
+
+
+export const main = (): void => {
+  logger(setZeroes, [[0, 1]]);
+}
