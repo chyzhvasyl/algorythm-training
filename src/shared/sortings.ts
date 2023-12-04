8b1a4b6ace746d071bcfc7e069f21e5f283a3b71
Vasyl Chyzh <v.chyzh@levi9.com>
2023-12-04 09:53:59 +0200

    Frequency of the Most Frequent Element: fails on big data

---
+++ src/shared/sortings.ts
@@ -0,0 +1,21 @@
+export const quickSort = (arr: number[]): number[] => {
+  if (arr.length <= 1) return arr;
+
+  const pivotIndex: number = Math.floor(arr.length / 2);
+  const pivot: number = arr[pivotIndex];
+
+  const less: number[] = [];
+  const greater: number[] = [];
+
+  for (let i: number = 0; i < arr.length; i++) {
+    if (i === pivotIndex) continue;
+
+    if (arr[i] < pivot) {
+      less.push(arr[i]);
+    } else {
+      greater.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(less), pivot, ...quickSort(greater)];
+}
