11fc818e680e58924cf98a2aba47140cbcd16848
vasylchyzh <vasyl.chyzh@3shape.com>
2024-10-22 17:22:53 +0300

    Linked List Cycle accepted

---
+++ src/leetcode/medium/Linked List Cycle/index.ts
@@ -0,0 +1,25 @@
+import { ListNode } from "../../../shared";
+
+export const cycledLinkedList = new ListNode(3);
+cycledLinkedList.next = new ListNode(2);
+cycledLinkedList.next.next = new ListNode(0);
+cycledLinkedList.next.next.next = new ListNode(4);
+cycledLinkedList.next.next.next.next = new ListNode(6);
+
+export const hasCycle = (head: ListNode): boolean => {
+  let slow: ListNode | null | undefined = head;
+  let fast: ListNode | null | undefined = slow?.next;
+
+  if (!fast) return false;
+
+  while (slow && fast) {
+    if (slow === fast) return true;
+
+    slow = slow?.next!;
+    fast = fast?.next?.next;
+  }
+
+  return false;
+};
+
+//Accepted
--- src/leetcode/medium/index.ts
+++ src/leetcode/medium/index.ts
@@ -2,6 +2,7 @@ import { logger } from '../../shared';
 import { setZeroes } from './Set Matrix Zeroes';
 import { maxFrequency2 } from './Frequency of the Most Frequent Element';
 import { getMyLinkedList } from "./Design Linked List";
+import {cycledLinkedList, hasCycle} from "./Linked List Cycle";
 
 
 export const main = (): void => {
@@ -10,4 +11,6 @@ export const main = (): void => {
   logger(maxFrequency2, [1,2,3], 1);
 
   logger(getMyLinkedList)
+
+  console.log('hasCycle', hasCycle(cycledLinkedList));
 }
